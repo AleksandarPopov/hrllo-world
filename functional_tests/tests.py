9ad31f4aa49581e37c04243be7ea5c026a38f914
Aleksandar Popov <sa6opopov94@gmail.com>
2014-08-14 15:08:49 +0300

    Hack FT runner to be able to test staging

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -4,6 +4,17 @@ from selenium.webdriver.common.keys import Keys
 
 class NewVisitorTest(StaticLiveServerCase):
 
+    def setUpClass(cls):
+    	    for arg in sys.argv:
+    	    	    if 'liveserver' in arg:
+    	    	    	    cls.server_url = 'http://' +arg.split('=')[1]
+    	    	    	    return 
+    	    	    super().setUpClass()
+    	    	    cls.server_url = cls.live_server_url
+    def tearDownClass(cls):
+    	    if cls.server_url == cls.live_server_url:
+    	    	    super().tearDownClass()
+
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
@@ -21,7 +32,7 @@ class NewVisitorTest(StaticLiveServerCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
 
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
@@ -67,7 +78,7 @@ class NewVisitorTest(StaticLiveServerCase):
 
         # Francis visits the home page.  There is no sign of Edith's
         # list
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertNotIn('make a fly', page_text)
@@ -92,7 +103,7 @@ class NewVisitorTest(StaticLiveServerCase):
         
     def test_layout_and_styling(self):
     	    
-	self.browser.get(self.live_server_url)
+	self.browser.get(self.server_url)
 	self.browser.set_window_size(1024,768)
 	
 	inputbox = self.browser.find_element_by_id('id_new_item')
